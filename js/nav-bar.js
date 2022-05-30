2424b7252dce6b661a0618180f867723e25fa642
chandan bauri <chandanbauri441@gmail.com>
2022-05-31 03:31:18 +0530

    updated the links and made the nav bar working

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -3,4 +3,11 @@ $(function () {
         var $nav = $(".nav-bar-outer-container");
         $nav.toggleClass('scrolling-nav', $(this).scrollTop() > $nav.height());
     });
+
+    $(".nav-option > a , .about-me-anchor").on('click', function () {
+        let section = $(this).attr('data-section');
+        $('html, body').animate({
+            scrollTop: $(`#${section}`).offset().top - 25
+        }, 1000);
+    });
 });
